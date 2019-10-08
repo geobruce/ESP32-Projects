49b1ec0d3ac62c684e63cf9be2f662cbc601e25f
geobruce <brucehelsen@hotmail.com>
2019-10-08 09:42:00 +0200

    Added seperate file for wifi credentials

---
+++ wifi_blink_stability_test/WiFiCredentials.h
@@ -0,0 +1,3 @@
+// Replace with your network credentials
+const char* ssid = "YourNetworkName";
+const char* password = "YourPassword";
---
+++ wifi_blink_stability_test/wifi_blink_stability_test.ino
@@ -0,0 +1,15 @@
+/*
+ * Bruce Helsen
+ * This code is to test the stability of the ESP32 and find a solution to recover from lost connections.
+ */
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
